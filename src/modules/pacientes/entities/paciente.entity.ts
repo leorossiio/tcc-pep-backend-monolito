81750b8f1e98e4e70e5a6feff78430809af9074b
Guilherme <gmoraes806@gmail.com>
2026-05-10 18:30:03 -0300

    feat: adiciona entidades TypeORM para o banco relacional (PG)
    A

---
+++ src/modules/pacientes/entities/paciente.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Atendimento } from '../../atendimentos/entities/atendimento.entity';
+
+@Entity('pacientes_pg')
+export class Paciente {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ name: 'nome_completo', type: 'varchar' })
+  nomeCompleto!: string;
+
+  @Column({ type: 'varchar' })
+  sexo!: string;
+
+  @Column({ name: 'cpf_hash', type: 'varchar' })
+  cpfHash!: string;
+
+  @Column({ name: 'data_nascimento', type: 'date' })
+  dataNascimento!: Date;
+
+  @Column({ name: 'telefone_contato', type: 'varchar', nullable: true })
+  telefoneContato!: string;
+
+  @Column({ name: 'tipagem_sanguinea', type: 'varchar', nullable: true })
+  tipagemSanguinea!: string;
+
+  @Column({ name: 'consentimento_lgpd', type: 'boolean' })
+  consentimentoLgpd!: boolean;
+
+  @CreateDateColumn({ name: 'criado_em', type: 'timestamp' })
+  criadoEm!: Date;
+
+  @OneToMany(() => Atendimento, (atendimento) => atendimento.paciente)
+  atendimentos!: Atendimento[];
+}
