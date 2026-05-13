5f3c4ef5f9171935f65efc022aa037a0a28598f6
Leonardo <leonardorossioliveira@gmail.com>
2026-05-13 00:38:13 -0300

    feat(logs-auditoria): configure module with TypeORM integration and service/controller setup

--- src/modules/logs-auditoria/logs-auditoria.module.ts
+++ src/modules/logs-auditoria/logs-auditoria.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LogAuditoria } from './entities/logs-auditoria.entity';
+import { LogsAuditoriaService } from './services/logs-auditoria.service';
+import { LogsAuditoriaRepository } from './repositories/logs-auditoria.repository';
+import { LogsAuditoriaController } from './controllers/logs-auditoria.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([LogAuditoria])],
+  controllers: [LogsAuditoriaController],
+  providers: [LogsAuditoriaService, LogsAuditoriaRepository],
+  exports: [LogsAuditoriaService],
+})
 export class LogsAuditoriaModule {}
